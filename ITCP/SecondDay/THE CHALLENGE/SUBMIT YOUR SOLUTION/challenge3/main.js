b445aa92db85397312ac3cdcdb1a00d7b3b99bf2
Yacine Abed <90479697+yacine-abed@users.noreply.github.com>
2022-12-26 10:03:17 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/challenge3/main.js
@@ -0,0 +1,8 @@
+function leapyear(year){
+    return year % 4==0 && (year % 100 !=0 || year % 400 !=0);
+}
+if(leapyear(1504)){
+    console.log("its a leap year")
+}else{
+    console.log("its not a leap year")
+}
