86278299fc5dc0666eba2cd8880197ac3a9cd0b3
Yacine Abed <90479697+yacine-abed@users.noreply.github.com>
2022-12-26 12:37:03 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/challenge22/main.js
@@ -0,0 +1,34 @@
+const morseCode = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+ }
+ const convertToMorse = (str) => {
+    return str.toUpperCase().split("").map(el => {
+       return morseCode[el] ? morseCode[el] : el;
+    }).join("");
+ };
+ console.log(convertToMorse('Disaster management'));
+ console.log(convertToMorse('hey there!'));
